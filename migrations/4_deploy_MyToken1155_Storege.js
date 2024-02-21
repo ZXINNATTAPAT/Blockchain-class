21463152d284a5085bff3ee98a5f1b87e51cdbb7
Nattapatscz <bigboyzin88@gmail.com>
2024-02-22 00:04:00 +0700

    check account

--- migrations/4_deploy_MyToken1155_Storege.js
+++ migrations/4_deploy_MyToken1155_Storege.js
@@ -2,7 +2,9 @@ const MyToken1155 =  artifacts.require("MyERC1155");
 
 module.exports = function(deployer) {
     account = "0x229C0c5Da0A6595F652639271297C8Ee8324Ae52";
+    
     urls = 'http://localhost:3000';
     deployer.deploy(MyToken1155,urls,account);
+    
   };
   
